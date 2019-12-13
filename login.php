4a69547f96bc64b6a60abb554519dcf8d877b267
andy94077 <andy94077@gmail.com>
2019-12-13 18:13:02 +0800

    Restore login.php

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+	ob_start();
+   	session_start();
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	//error_reporting(-1);	
+
+	if(file_exists('passwd'))
+		$dict = json_decode(file_get_contents('passwd', 'r'), true);
+	else
+		$dict = array();
+
+	if ($_POST['type'] == 'login'){
+		if(array_key_exists($_POST['username'], $dict) && $_POST['password'] == $dict[$_POST['username']]) {
+			$_SESSION['valid'] = true;
+			$_SESSION['timeout'] = time();
+			$_SESSION['username'] = $_POST['username'];
+
+			echo 'success';
+		}
+		else
+			echo 'Wrong username or password';
+	}
+	else if(empty($_POST['username']))
+		echo 'Username cannot be empty';
+	else if(array_key_exists($_POST['username'], $dict))
+		echo 'Username has already existed';
+	else {
+		$dict[$_POST['username']] = $_POST['password'];
+		echo 'Signed up!';
+	}
+
+	file_put_contents('passwd', json_encode($dict))
+?>
+
