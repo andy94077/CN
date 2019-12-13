dfef06cebba724e662e8d3e1612fee2602383820
andy94077 <andy94077@gmail.com>
2019-12-13 18:13:24 +0800

    Prevent html parsing in send.php

--- send.php
+++ send.php
@@ -12,6 +12,6 @@
 	else
 		$chat_file_path = 'chat/'.$_POST['to_user'].'_'.$_POST['from_user'];
 
-	file_put_contents($chat_file_path, $_POST['from_user'].' '.bin2hex($_POST['message'])."\n", FILE_APPEND);
+	file_put_contents($chat_file_path, $_POST['from_user'].' '.bin2hex(str_replace(array('&', '<', '>'), array('&amp;', '&lt;', '&gt;'), $_POST['message']))."\n", FILE_APPEND);
 	echo '';
 ?>
