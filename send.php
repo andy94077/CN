5362bc9a5bddf9cc321d76a4b00a0ea448dce6cc
andy94077 <andy94077@gmail.com>
2019-12-19 17:28:30 +0800

    Improve security

--- send.php
+++ send.php
@@ -7,10 +7,18 @@
 	if(!$_SESSION['valid'])
 		header('Location: index.php');
 	
-	if(strcmp($_POST['from_user'], $_POST['to_user']) < 0)
-		$chat_file_path = 'chat/'.$_POST['from_user'].'_'.$_POST['to_user'];
+	if(file_exists('passwd'))
+		$dict = json_decode(file_get_contents('passwd'), true);
 	else
-		$chat_file_path = 'chat/'.$_POST['to_user'].'_'.$_POST['from_user'];
+		$dict = array();
+
+	if ($_POST['from_user'] != $_SESSION['username'] || !array_key_exists($_POST['to_user'], $dict))
+		return;
+
+	if(strcmp($_POST['from_user'], $_POST['to_user']) < 0)
+		$chat_file_path = 'chat/'.$_POST['from_user'].'|'.$_POST['to_user'];
+	else
+		$chat_file_path = 'chat/'.$_POST['to_user'].'|'.$_POST['from_user'];
 
 	file_put_contents($chat_file_path, $_POST['from_user'].' '.bin2hex(str_replace(array('&', '<', '>'), array('&amp;', '&lt;', '&gt;'), $_POST['message']))."\n", FILE_APPEND);
 	echo '';
--- upload.php
+++ upload.php
@@ -7,6 +7,14 @@
 	if(!$_SESSION['valid'])
 		header('Location: index.php');
 	
+	if(file_exists('passwd'))
+		$dict = json_decode(file_get_contents('passwd'), true);
+	else
+		$dict = array();
+
+	if ($_POST['from_user'] != $_SESSION['username'] || !array_key_exists($_POST['to_user'], $dict))
+		return;
+
 	if($_FILES['file']['error'] == UPLOAD_ERR_NO_FILE){
 		http_response_code(422);
 		echo 'The file does not exist.';
@@ -17,19 +25,18 @@
 		echo 'File size must be no more than 11 MB.';
 		return;
 	}
-
 	if(strcmp($_POST['from_user'], $_POST['to_user']) < 0){
-		$file_dir = 'upload_files/'.$_POST['from_user'].'_'.$_POST['to_user'];
-		$chat_file_path = 'chat/'.$_POST['from_user'].'_'.$_POST['to_user'];
+		$file_dir = 'upload_files/'.$_POST['from_user'].'|'.$_POST['to_user'];
+		$chat_file_path = 'chat/'.$_POST['from_user'].'|'.$_POST['to_user'];
 	}
 	else{
-		$file_dir = 'upload_files/'.$_POST['to_user'].'_'.$_POST['from_user'];
-		$chat_file_path = 'chat/'.$_POST['to_user'].'_'.$_POST['from_user'];
+		$file_dir = 'upload_files/'.$_POST['to_user'].'|'.$_POST['from_user'];
+		$chat_file_path = 'chat/'.$_POST['to_user'].'|'.$_POST['from_user'];
 	}
 
 	if(!file_exists($file_dir)){
 		mkdir($file_dir);
-		chmod($file_dir, 0755);
+		chmod($file_dir, 0711);
 	}
 
 	$name = $file_dir.'/'.hash('sha256', $_FILES['file']['tmp_name']).time();
